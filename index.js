a0916b0829ece805fe7814e231d50cf0acd8c981
Alex Step <stepalexandr@gmail.com>
2017-09-26 11:21:05 +0300

    oppress,_read,pipe(process.stdout)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+/*
 console.log(`\n\t${__dirname}`);
 console.log(`\t${__filename}\n`);
+*/
 
 const assert = require('assert');
 const Stream = require('stream');
 const http = require('http');
 const fs = require('fs');
+const oppressor = require('oppressor');
 
 const server = http.createServer( (request, response) => {
 
+// Read file stream for web-server
     /*fs.readFile(`${__dirname}/package.json`, {}, (err, data) => {
         assert.equal(err,null,'File reading error');
         response.setHeader('Content-Type', 'text/html');
@@ -17,10 +21,10 @@ const server = http.createServer( (request, response) => {
         `);
     } );*/
 
-
-    let stream = fs.createReadStream(`${__dirname}/package.json`);
-    stream.pipe(response);
-
+// Readfile stream for web-server with/without compressing. Stream's events
+    /*let stream = fs.createReadStream(`${__dirname}/package.json`);
+    // stream.pipe(response);
+    // stream.pipe(oppressor(request)).pipe(response); // compress
 
     stream.on('data', (chunk) => {
         console.log(chunk.toString())
@@ -39,8 +43,30 @@ const server = http.createServer( (request, response) => {
 
     stream.on('error', () => {
         console.log('error')
-    });
+    });*/
 
 } );
+// server.listen(3000);
 
-server.listen(3000);
+// Work with streams. Types of them
+let readableStream$ = new Stream.Readable();
+
+// pre buffer
+// readableStream$.push('test\n');
+// readableStream$.push(null); // finish of stream
+
+// handler on read event (handler wait before something begin to read the stream)
+let charCode = 'a'.charCodeAt(0);
+readableStream$._read = function () {
+    readableStream$.push(String.fromCharCode(charCode++));
+    if( charCode > 'z'.charCodeAt(0) ) { readableStream$.push('\n'); readableStream$.push(null) }
+};
+
+process.on('exit', function () {
+    console.error(`Was sent in stream ${charCode-'a'.charCodeAt(0)} letters`);
+});
+process.stdout.on('error', process.exit);
+
+setTimeout( function () {
+    readableStream$.pipe(process.stdout);
+} , 1000);
