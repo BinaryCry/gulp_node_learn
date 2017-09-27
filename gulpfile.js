ef2dc0fc23097cadf1d4b309533847b3805e900f
Alex Step <stepalexandr@gmail.com>
2017-09-27 18:20:12 +0300

    dest

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,39 @@ const gulp = require('gulp');
 gulp.task('hello', (arg) => {
 
     console.log('\n\tHello World!\n');
-    arg();
+    arg(); // callaback for off task
 
 });
 
 
 gulp.task('default', function (arg) {
 
-    console.log('default');
+    return gulp.src('src/**/*.*')
+        .on('data', function (file) {
+
+
+            console.log(
+                {
+                    contents: file.contents,
+                    path: file.path,
+                    cwd: file.cwd,
+                    base: file.base,
+                    relative: file.relative,
+                    dirname: file.dirname,
+                    basename: file.basename,
+                    stem: file.stem,
+                    extname: file.extname,
+                }
+            );
+            console.log('')
+
+
+        })
+        .pipe(gulp.dest( function (file) {
+
+            return file.extname == '.js' ? 'dest/js' :
+                   file.extname == '.css' ? 'dest/css' : 'dest'
+
+        } ))
 
 });
