5bf640c4df9da6a956b15131c572389b96739846
Alex Step <stepalexandr@gmail.com>
2017-09-28 18:00:29 +0300

    28_09

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ console.log( minimatch('log.txt', "*.*") );
 
 
 let fileList = ['a.js', 'b.css', 'c.jsx', 'd.js'];
-let f = minimatch.filter("*.js", {matchBase: true});
+let f = minimatch.filter("*.js", { matchBase: true });
 
 console.log( fileList.map(f) );
