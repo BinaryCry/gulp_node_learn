ac8bb69b77ae590c2cbcffbdfa5c1176ac459afa
Alex Step <stepalexandr@gmail.com>
2017-09-28 15:00:12 +0300

    4v, minimatch

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const minimatch = require('minimatch');
+
+console.log( minimatch('log.txt', "*.*") );
+
+
+let fileList = ['a.js', 'b.css', 'c.jsx', 'd.js'];
+let f = minimatch.filter("*.js", {matchBase: true});
+
+console.log( fileList.map(f) );
